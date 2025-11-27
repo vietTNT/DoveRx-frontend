362a0f7d6079a5e2bb3f61ab1ad5d850932b4c26
vietTNT <trandacdaiviet@gmail.com>
2025-11-28 01:06:20 +0700

    test wed app chrome

--- src/index.js
+++ src/index.js
@@ -12,3 +12,12 @@ root.render(
 );
 
 reportWebVitals();
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => console.log("SW registered"))
+      .catch((err) => console.log("SW registration failed: ", err));
+  });
+}
